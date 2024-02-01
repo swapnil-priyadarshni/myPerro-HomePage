237a9d0d01bc9b5e2927f8f180613d2f0ccb1403
swapnil-priyadarshni <143959494+swapnil-priyadarshni@users.noreply.github.com>
2024-02-01 22:27:54 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+const multipleItemCarousel = document.querySelector('#carouselExampleControls')
+
+
+if (window.matchMedia ("(min-width:576px)").matches){
+    const carousel = new bootstrap.Carousel(multipleItemCarousel, {
+        interval: false
+      })
+
+var carouselWidth = $(".carousel-inner")[0].scrollWidth;
+var cardWidth = $(".carousel-item").width();
+var scrollPosition = 0;
+
+$(".carousel-control-next").on("click", function () {
+    if (scrollPosition < (carouselWidth - cardWidth * 4)) { 
+      scrollPosition += cardWidth;  
+      $(".carousel-inner").animate({ scrollLeft: scrollPosition },600); 
+    }
+  });
+
+  $(".carousel-control-prev").on("click", function () {
+    if (scrollPosition > 0) {
+      scrollPosition -= cardWidth;
+      $(".carousel-inner").animate(
+        { scrollLeft: scrollPosition },
+        600
+      );
+    }
+  });
+} else{
+    $(multipleCardCarousel).addClass("slide");
+}
